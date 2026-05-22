a4e537a2cf2ef8691473e537b6b6d1382e1d0ff0
BrianMath <brianmatheus10@gmail.com>
2026-05-22 17:39:18 -0300

    feat: new examples

---
+++ Go/factorial.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	ch := make(chan int)
+	go fact(5, ch)
+}
+
+func fact(n int, results chan<- int) {
+	if n <= 1 {
+		results <- n
+		return
+	}
+	ch := make(chan int)
+	go fact(n-1, ch)
+	results <- n * <-ch
+}
