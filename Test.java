02838d423fd9ca70dc5392a12c962370013f59b4
BrianMath <brianmatheus10@gmail.com>
2026-05-22 17:42:38 -0300

    update: initial generation from func rule

--- Test.java
+++ Test.java
@@ -1,13 +1,42 @@
-public class Test extends GolzinhoBaseListener {
-	@Override
-	public void enterPackage(GolzinhoParser.PackageContext ctx) {
-		System.out.println(ctx.ID().getText());
-	}
+import java.util.ArrayList;
 
+public class Test extends GolzinhoBaseListener {
 	@Override
 	public void enterAssignChan(GolzinhoParser.AssignChanContext ctx) {
 		String chan = ctx.ID().getText();
 		chan = "(ν " + chan + chan + "'" + ")";
-		System.out.println(chan);
+		System.out.print(chan);
+	}
+
+	@Override
+	public void enterFunc(GolzinhoParser.FuncContext ctx) {
+		ArrayList<String> params = new ArrayList<String>();
+		GolzinhoParser.ParamContext first = ctx.param();
+		if (first != null) {
+			params.add(first.ID().getText());
+			for (GolzinhoParser.ParamContext p : first.param()) {
+				params.add(p.ID().getText());
+			}
+		}
+
+		String funcName = ctx.ID().getText();
+
+		if (funcName.equals("main")) {
+			return;
+		}
+
+		System.out.println(
+			"def " + funcName + "(" + printParams(params) + ") = ... in"
+		);
+	}
+
+	private String printParams(ArrayList<String> params) {
+		if (params.size() == 0) return "";
+
+		String paramList = params.getFirst();
+		for (int i = 1; i < params.size(); i++) {
+			paramList += " " + params.get(i);
+		}
+		return paramList;
 	}
 }
